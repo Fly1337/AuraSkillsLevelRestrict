53a625d8f8c8df5a6ef5e1c8d341a05b681fbbbf
Fly1337 <tobi.daemon1337@gmail.com>
2024-05-16 13:19:08 +0200

    Initial commit

---
+++ src/main/java/com/fly/auraskillslevelrestrict/Addon.java
@@ -0,0 +1,17 @@
+package com.fly.auraskillslevelrestrict;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Addon extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/com/fly/auraskillslevelrestrict/Listener.java
@@ -0,0 +1,2 @@
+package com.fly.auraskillslevelrestrict;public class Listener {
+}
